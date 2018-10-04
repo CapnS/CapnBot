6f766d49653f38f34628a377639785812c5b1bb9
CapnS <capnsurvivalist@gmail.com>
2018-10-04 15:07:46 -0500

    Undid Reverse

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -68,7 +68,6 @@ class Twitter():
             amount = (- 1)
             followers = api.followers_ids('TRGCapn')
             friends = api.friends_ids('TRGCapn')
-            friends = friends[::-1]
             for f in friends:
                 if f not in followers:
                     amount = int(amount) + 1
